c83b690d7b05c68740d2d8dc1e76d4ebe30713e4
fellipe.marra <fellipe.marra@sankhya.com.br>
2024-12-10 14:00:00 -0300

    Ajuste de cors

--- src/main/java/com/esocial/script/generator/config/WebSecurityConfig.java
+++ src/main/java/com/esocial/script/generator/config/WebSecurityConfig.java
@@ -2,12 +2,9 @@ package com.esocial.script.generator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,24 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build());
-        return manager;
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll()
                 )
-                .csrf().disable()
-                .httpBasic(Customizer.withDefaults());
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
